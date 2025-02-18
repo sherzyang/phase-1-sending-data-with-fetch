e559e30ebec70ac7b31d778e12c4cb79ca740382
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-02-18 13:07:21 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+const submitData = (name, email) => {
+    const formData = {
+        name: name,
+        email: email,
+    };
+  
+    const configurationObject = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function(data){
+        obj = data.id
+        document.body.innerHTML += obj
+    })
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+    });
+}
+
